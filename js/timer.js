10f52751abbaedbf52701825bac88a1ff30e6aa3
MayckellP <mayckell-perez@hotmail.com>
2022-12-08 14:35:28 +0100

    Audio fixed

--- js/timer.js
+++ js/timer.js
@@ -36,6 +36,7 @@ validation = () => {
     saveDate();
   }
 };
+
 saveDate = () => {
   countDownDate = new Date(
     `${month.value} ${day.value}, ${year.value}, ${hour.value}:${minute.value}:${second.value}`
@@ -117,6 +118,7 @@ defaultTime = () => {
   hour.value = countDownDate.getHours();
   minute.value = countDownDate.getMinutes() + 1;
 };
+defaultTime();
 var pauseTimer = () => {
   clearInterval(x);
   clearInterval(intervalId);
@@ -132,6 +134,7 @@ var pauseTimer = () => {
     "cont-fire"
   ).innerHTML = `<img  src="images/congratulation.png" style="width: 100%;">`;
 };
+
 var oppo = 3;
 boom = () => {
   oppo--;
@@ -152,6 +155,10 @@ boom = () => {
 };
 
 nuevoDefault = () => {
+  const timer = document.getElementById("circle");
+
+  var sound = document.getElementById("aud");
+  console.log(sound);
   var count;
   var maxCount = 20;
 
@@ -182,152 +189,21 @@ nuevoDefault = () => {
             <td>SECONDS</td>
           </tr>
         </table>`;
-    if (info <= 10) {
+    if (info < 10 && info > 7) {
       timer.style.backgroundColor = "rgb(102, 35, 35)";
-      document.getElementById("audio").onplay();
-    }
-    if (info < 7) {
+      console.log(info);
+    } else if (info < 8 && info > 4) {
       timer.style.backgroundColor = "brown";
-    }
-    if (info < 4) {
+      sound.play();
+      console.log(info);
+    } else if (info < 4 && info > 1) {
       timer.style.backgroundColor = "rgb(78, 22, 22)";
-    }
-    if (info < 0) {
+    } else if (info < 1) {
+      console.log(info);
       clearInterval(intervalId);
       document.getElementById(
         "cont-fire"
-      ).innerHTML = `<img id="boomfin" class="m-auto" src="images/explode.png">`;
+      ).innerHTML = `<img src="images/explode.png" style="width: 100%;">`;
     }
   }
 };
-
-/*var newTimer;
-reloadTimer = () => {
-  //information from stopping point
-
-  //add this inf to
-  var distance = countDownDate - nowCopy;
-  newTimer;
-  const initialDay = Math.floor(distance / (1000 * 60 * 60 * 24));
-  const initialHour = Math.floor(
-    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-  );
-  const initialMinute = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  const initialSecond = Math.floor((distance % (1000 * 60)) / 1000);
-
-  const capture_day = days;
-  const capture_hour = hours;
-  const capture_minute = minutes;
-  const capture_second = seconds;
-
-  var fyear = parseInt(year.value);
-  var fmonth = parseInt(month.value);
-  var fday = parseInt(day.value);
-  var fhour = parseInt(hour.value) - initialHour;
-  var fminute = parseInt(minute.value) - initialMinute;
-  var fsecond = parseInt(second.value) - initialSecond;
-
-  var resfyear = fyear;
-  var resfmonth = fmonth;
-  var resfday = fday;
-  var resfhour = fhour + capture_hour;
-  var resfminute = fminute + capture_minute;
-  var resfsecond = fsecond + capture_second;
-
-  const resDay = initialDay - capture_day;
-  const resHour = initialHour - capture_hour;
-  const resMin = initialMinute - capture_minute;
-  const resSec = initialSecond - capture_second;
-
-  var newDay = day.value;
-  var newHour = parseInt(hour.value) - resHour;
-  var newMin = parseInt(minute.value) - resMin;
-  var newSec = parseInt(second.value) - resSec;
-  var newYear = year.value;
-  var newMonth = month.value;
-
-  console.log("/-------------------------------------");
-  console.log(
-    `Fecha 1 Tope ${month.value} ${day.value}, ${year.value}, ${hour.value}:${minute.value}:${second.value}`
-  );
-  console.log(
-    `Distancia Total: ${initialDay}, ${initialHour}:${initialMinute}:${initialSecond}`
-  );
-  console.log(
-    `Tiempo Pausado: ${capture_day}, ${capture_hour}:${capture_minute}:${capture_second}`
-  );
-  console.log(`Tiempo Consumido: ${resDay}, ${resHour}:${resMin}:${resSec}`);
-  console.log(
-    `Fecha 2 Tope: ${newMonth} ${newDay}, ${newYear}, ${newHour}:${newMin}:${newSec}`
-  );
-  console.log(
-    `Tiempo a Sumar: ${fmonth} ${fday}, ${fyear}, ${fhour}:${fminute}:${fsecond}`
-  );
-  console.log(
-    `Fecha Inicial: ${resfmonth} ${resfday}, ${resfyear}, ${resfhour}:${resfminute}:${resfsecond}`
-  );
-
-  newTimer = new Date(
-    `${month.value} ${day.value}, ${year.value}, ${newHour}:${newMin}:${newSec}`
-  ).getTime();
-
-  console.log("Fecha Inicial: " + nowCopy);
-  console.log("Fecha Tope(NEW INPUT) " + newTimer);
-
-  y = setInterval(function () {
-    var newInital = new Date(
-      `${resfmonth}-${resfday}-${resfyear}, ${resfhour}:${resfminute}:${resfsecond}`
-    ).getTime();
-    var instance = newTimer - newInital;
-
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(instance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor(
-      (instance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
-
-    var minutes = Math.floor((instance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((instance % (1000 * 60)) / 1000);
-
-    var testmsg = `day ${days}, hr ${hours}, min ${minutes}, sec ${seconds}`;
-    console.log(testmsg);
-    // Output the result in an element with id="demo"
-    document.getElementById("tbl-out").innerHTML = `
-          <table class="tbl-out">
-                  <tr id="output">
-                    <td>${days}</td>
-                    <td>${hours}:</td>
-                    <td>${minutes}:</td>
-                    <td>${seconds}</td>
-                  </tr>
-                  <tr class="base">
-                    <td>DAYS</td>
-                    <td>HOURS</td>
-                    <td>MINUTES</td>
-                    <td>SECONDS</td>
-                  </tr>
-                </table>`;
-    // If the count down is over, write some text
-    if (days == 0 && hours == 0 && minutes == 0 && seconds < 10) {
-      const timer = document.getElementById("circle");
-      timer.style.backgroundColor = "rgb(131, 51, 51)";
-      console.log("funcion1");
-    }
-    if (days == 0 && hours == 0 && minutes == 0 && seconds < 7) {
-      const timer = document.getElementById("circle");
-      timer.style.backgroundColor = "rgb(102, 35, 35)";
-      console.log("funcion2");
-    }
-    if (days == 0 && hours == 0 && minutes == 0 && seconds < 4) {
-      const timer = document.getElementById("circle");
-      timer.style.backgroundColor = "rgb(78, 22, 22)";
-      console.log("funcion3");
-    }
-    /*if (instance < 1) {
-      clearInterval(y);
-      document.getElementById(
-        "cont-fire"
-      ).innerHTML = `<img class="m-auto" src="images/explode.png" alt="">`;
-    }
-  }, 1000);
-};*/
